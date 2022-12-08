de02374a528e887455a58151037b0a0ff5d08da6
mrbluesky1 <76676130+mrbluesky1@users.noreply.github.com>
2022-12-08 21:34:52 +0600

    Add files via upload

---
+++ DZ2_Emir_21-01/main.js
@@ -0,0 +1,15 @@
+var num = []
+num.push(num=10)
+for (num = 20; num > 1; num --)
+    console.log(num)
+
+var svetofor = prompt('Введите свет светофора')
+if (svetofor === 'красный') {
+    alert('Стоп')
+} else  if (svetofor === 'желтый') {
+    alert('Приготовься')
+} else if (svetofor === 'зеленый') {
+    alert('Вперед')
+} else {
+    alert('Красный, желтый или зеленый')
+}
